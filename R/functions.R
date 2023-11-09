1d15fbe50d66067209b7d4845ca664d6927495af
barbaroseroglu <barbaroseroglu@gmail.com>
2023-11-09 10:55:54 +0100

    plotting function added

--- R/functions.R
+++ R/functions.R
@@ -10,3 +10,16 @@ descriptive_stats <- function(data) {
         dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
         dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
 }
+
+#' Visualizing the lipidomics data
+#'
+#' @param lipidomics dataset
+#'
+#' @return plots of each lipid
+#'
+plot_distributions <- function(data) {
+    ggplot(data,
+           aes(x = value)) +
+        geom_histogram() +
+        facet_wrap(vars(metabolite), scales = "free")
+}
