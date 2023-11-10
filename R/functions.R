7b070c42f2814b4ff213c95778dc7a7029feaee3
barbaroseroglu <barbaroseroglu@gmail.com>
2023-11-10 09:58:29 +0100

    new functions added

--- R/functions.R
+++ R/functions.R
@@ -3,7 +3,6 @@
 #' @param data lipidomics dataset
 #'
 #' @return "A data.frame/tibble"
-
 descriptive_stats <- function(data) {
   data %>%
     dplyr::group_by(metabolite) %>%
@@ -16,7 +15,6 @@ descriptive_stats <- function(data) {
 #' @param lipidomics dataset
 #'
 #' @return plots of each lipid
-#'
 plot_distributions <- function(data) {
   data %>% ggplot2::ggplot(ggplot2::aes(x = value)) +
     ggplot2::geom_histogram() +
@@ -30,8 +28,7 @@ plot_distributions <- function(data) {
 #' @param cols The column to be converted into snakecase
 #'
 #' @return a dataframe
-#'
-column_values_to_snakecase <- function(data, cols) {
+column_values_to_snake_case <- function(data, cols) {
   data %>%
     dplyr::mutate(dplyr::across({{ cols }}, snakecase::to_snake_case))
 }
@@ -42,7 +39,6 @@ column_values_to_snakecase <- function(data, cols) {
 #' @param the lipidomics dataset
 #'
 #' @return a wide dataframe
-
 metabolites_to_wider <-  function(data) {
     data %>%
     tidyr::pivot_wider(
@@ -59,7 +55,6 @@ metabolites_to_wider <-  function(data) {
 #' @param the column of the metabolite variable
 #'
 #' @return a dataframe
-
 create_recipe_spec <- function(data, metabolite_variable) {
     recipes::recipe(data) %>%
         recipes::update_role({{ metabolite_variable}}, age, gender, new_role = "predictor") %>%
@@ -73,7 +68,6 @@ create_recipe_spec <- function(data, metabolite_variable) {
 #' @param recipe_specs the recipe specifications
 #'
 #' @return a workflow object
-
 create_model_workflow <- function(model_specs, recipe_specs) {
     workflows::workflow() %>%
         workflows::add_model(model_specs) %>%
@@ -91,4 +85,50 @@ tidy_model_output <- function(workflow_fitted_model) {
     workflow_fitted_model %>%
         workflows::extract_fit_parsnip() %>%
         broom::tidy(exponentiate = TRUE)
+}
+
+#' Convert the long form dataset into a list of wide form dataframes
+#'
+#' @param data lipidomics
+#'
+#' @return a list of data frames
+#'
+split_by_metabolite <- function(data) {
+    data %>%
+        column_values_to_snake_case(metabolite) %>%
+        dplyr::group_split(metabolite) %>%
+        purrr::map(metabolites_to_wider)
+}
+
+#' Generate the results of the model
+#'
+#' @param data The lipidomics dataset
+#'
+#' @return A data frame
+generate_model_results <- function(data) {
+    create_model_workflow(
+        parsnip::logistic_reg() %>%
+            parsnip::set_engine("glm"),
+        data %>%
+            create_recipe_spec(tidyselect::starts_with("metabolite_"))
+    ) %>%
+        parsnip::fit(data) %>%
+        tidy_model_output()
+}
+
+
+#' Presenting model results for each metabolite in one table
+#'
+#' @param model_results
+#' @param data the lipidomics dataset
+#'
+#' @return a data frame
+add_original_metabolite_names <- function(model_results, data) {
+    data %>%
+        dplyr::select(metabolite) %>%
+        dplyr::mutate(term = metabolite) %>%
+        column_values_to_snake_case() %>%
+        dplyr::mutate(term = stringr::str_c("metabolite_", term)) %>%
+        dplyr::distinct(term, metabolite) %>%
+        dplyr::right_join(model_results, by = "term" )
 }
