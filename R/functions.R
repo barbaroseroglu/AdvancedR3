6a98e49695688a434245c8bedb72bad4dd7946eb
barbaroseroglu <barbaroseroglu@gmail.com>
2023-11-10 11:16:14 +0100

    new function/target added

--- R/functions.R
+++ R/functions.R
@@ -124,13 +124,12 @@ generate_model_results <- function(data) {
 #'
 #' @return a data frame
 add_original_metabolite_names <- function(model_results, data) {
-  data %>%
-    dplyr::select(metabolite) %>%
-    dplyr::mutate(term = metabolite) %>%
-    column_values_to_snake_case() %>%
-    dplyr::mutate(term = stringr::str_c("metabolite_", term)) %>%
-    dplyr::distinct(term, metabolite) %>%
-    dplyr::right_join(model_results, by = "term")
+    data %>%
+        dplyr::mutate(term = metabolite) %>%
+        column_values_to_snake_case(term) %>%
+        dplyr::mutate(term = stringr::str_c("metabolite_", term)) %>%
+        dplyr::distinct(term, metabolite) %>%
+        dplyr::right_join(model_results, by = "term")
 }
 
 #' Calculate the estimates for each metabolite
@@ -140,10 +139,29 @@ add_original_metabolite_names <- function(model_results, data) {
 #' @return a data frame
 calculate_estimates <- function(data) {
     data %>%
-        split_by_metabolite() %>%
+        column_values_to_snake_case(metabolite) %>%
+        dplyr::group_split(metabolite) %>%
+        purrr::map(metabolites_to_wider) %>%
         purrr::map(generate_model_results) %>%
         purrr::list_rbind() %>%
         dplyr::filter(stringr::str_detect(term, "metabolite_")) %>%
         add_original_metabolite_names(data)
+}
+
+#' Plot the estimates and standard errors of the model results.
+#'
+#' @param results The model estimate results.
+#'
+#' @return A ggplot2 figure.
+#'
+plot_estimates <- function(results) {
+    results %>%
+        ggplot2::ggplot(ggplot2::aes(
+            x = estimate, y = term,
+            xmin = estimate - std.error,
+            xmax = estimate + std.error
+        )) +
+        ggplot2::geom_pointrange() +
+        ggplot2::coord_fixed(xlim = c(0, 5))
 }
 
